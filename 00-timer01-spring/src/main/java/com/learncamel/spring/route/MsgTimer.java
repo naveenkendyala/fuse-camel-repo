380f04f99b68d1abb70e9a0d6353181fc8efa47a
Naveen Kendyala <subscribeken@gmail.com>
2020-05-13 22:24:13 -0400

    created the route and tested

--- 00-timer01-spring/src/main/java/com/learncamel/spring/route/MsgTimer.java
+++ 00-timer01-spring/src/main/java/com/learncamel/spring/route/MsgTimer.java
@@ -1,13 +1,16 @@
 package com.learncamel.spring.route;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 public class MsgTimer extends RouteBuilder{
 
     @Override
     public void configure() throws Exception {
-        
 
+        //Start the route with the periodic timer
+        from("timer:msgtimer?fixedRate=true&period=1000")
+        .log(LoggingLevel.INFO, "Hello World !");
     }
     
 }
